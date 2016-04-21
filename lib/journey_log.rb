a3872d87f8dea162ddcab65e5c53e4740cbdcc78
Ruta <ruta@baltiejus.com>
2016-04-21 11:35:40 +0100

    some journey log stuff

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,10 +1,26 @@
+require_relative = "journey"
 class JourneyLog
 
   attr_reader :journey_class
 
-  def initialize(journey_class)
+  def initialize(journey_class = Journey)
     @journey_class = journey_class
+    @journeys = []
   end
-  
+
+  def start(entry_station)
+    @journeys << journey_class.new(entry_station)
+  end
+
+  def finish(exit_station)
+    @journeys << exit_station
+  end
+
+private
+
+  def current_journey
+    journey_class.new unless !@journey.empty?
+    @journey.keys[0]
+  end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,23 +1,23 @@
 require "journey_log"
 
 describe JourneyLog do
+  let(:journey) { double :journey}
+  let(:station) { double :station}
+  let(:journey_class) { double :journey_class, new: journey }
+  # let(:journey){ {entry_station: entry_station, exit_station: exitstation} }
+  # subject { JourneyLog.new journey_class }
 
-  let(:journey_class) { double :journey_class }
-  subject { JourneyLog.new journey_class }
-
-  it "initializes with a journey_class" do
+  xit "initializes with a journey_class" do
     expect(subject.journey_class).to eq journey_class
   end
 
   describe "#start" do
-    it ""
-
+    it "starts a new journey" do
+      expect(journey_class).to receive(:new).with(entry_station: station)
+      subject.start(station)
+    end
   end
-
-  describe "#current_journey" do
-
-  end
-
+  
   describe "#finish" do
 
   end
